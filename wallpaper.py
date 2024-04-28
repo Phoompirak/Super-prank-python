1f80184b41e202995e8e5efaaa0213214cf8c467
Phoompirak <rakpumpum@gmail.com>
2024-04-28 11:22:32 +0700

    Add git ignore

--- wallpaper.py
+++ wallpaper.py
@@ -4,15 +4,16 @@ from pyautogui import hotkey
 import time
 # import 'Virus-prank\Kazuya.jpg'
 
-path_image = 'Virus-prank\Kazuya.jpg'
+path_image = f'{os.getcwd()}\Kazuya.jpg'
+print(path_image)
 if os.path.exists(path_image):
     print("Found file wallpaper")
 else:
     print("Not found file wallpaper!!")
 ctypes.windll.user32.SystemParametersInfoW(20, 0, path_image, 0)
 
-pyautogui.hotkey("winleft", "m")
+# pyautogui.hotkey("winleft", "m")
 
-time.sleep(4)
+time.sleep(5)
 wall_default = r"C:\Windows\Web\Wallpaper\Windows\img19.jpg"
 ctypes.windll.user32.SystemParametersInfoW(20, 0, wall_default, 0)
